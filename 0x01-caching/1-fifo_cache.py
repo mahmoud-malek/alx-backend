b23627acab3673036520ce4f74856956bee9f842
mahmoud malek <information.mahmoud@gmail.com>
2024-09-03 22:03:13 +0300

    add\udpate files :fire:

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+""" a class FIFOCache that inherits
+from BaseCaching and is a caching system:
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ a class that defines a basic
+    caching algorithm """
+
+    def __init__(self):
+        """ initiative function that inherits from
+        base """
+        super().__init__()
+        self.data_order = []
+
+    def put(self, key, item):
+        """ a function to put the value in
+        the caching system """
+        if key is None or item is None:
+            return
+
+        if (len(self.cache_data) >= self.MAX_ITEMS):
+            deleted = self.data_order.pop(0)
+            del self.cache_data[deleted]
+            print(f"DISCARD: {deleted}")
+
+        self.cache_data[key] = item
+        self.data_order.append(key)
+
+    def get(self, key):
+        """ a function to get the cache value """
+        if key is None:
+            None
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
